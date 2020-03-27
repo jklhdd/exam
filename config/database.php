03714acb4cab90ed4abe5772213e56ed0978ed3b
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-03-27 16:05:06 +0700

    update

--- config/database.php
+++ config/database.php
@@ -65,17 +65,18 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            //'url' => env('DATABASE_URL'),
+            'host' => "ec2-35-168-54-239.compute-1.amazonaws.com", //env('DB_HOST', '127.0.0.1'),
+            'port' => 5432, //env('DB_PORT', '5432'),
+            'database' => "d10qjmlaabn7lp", //env('DB_DATABASE', 'forge'),
+            'username' => "ipymoyzaqrhxoh", //env('DB_USERNAME', 'forge'),
+            'password' =>
+                "37c9b44567ddf272e5173c548be24afec5783636dbd5feac2acc5413adfd716a", //env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'prefer'
         ],
 
         'sqlsrv' => [
